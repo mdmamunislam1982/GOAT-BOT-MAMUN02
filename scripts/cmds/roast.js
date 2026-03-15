0775ffe8be418eefea290c810f617a833799e668
Md Mamun Islam <mamunofficial3210@gmail.com>
2026-03-16 01:26:13 +0600

    Update and rename nigga.js to roast.js

---
+++ scripts/cmds/roast.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  config: {
+    name: "roast",
+    aliases: ["burn"],
+    version: "2.3",
+    author: "nexo_here",
+    countDown: 3,
+    role: 0,
+    description: "Send a roast image",
+    category: "fun"
+  },
+
+  onStart: async function ({ api, event }) {
+    try {
+
+      const mention = Object.keys(event.mentions || {});
+      const uid = mention[0] || event.senderID;
+
+      const cache = path.join(__dirname, "cache");
+      if (!fs.existsSync(cache)) fs.mkdirSync(cache, { recursive: true });
+
+      const img = path.join(cache, `roast_${Date.now()}.jpg`);
+
+      const res = await axios({
+        url: `https://betadash-api-swordslush-production.up.railway.app/nigga?userid=${uid}`,
+        method: "GET",
+        responseType: "stream"
+      });
+
+      const writer = fs.createWriteStream(img);
+      res.data.pipe(writer);
+
+      writer.on("finish", () => {
+        api.sendMessage(
+          {
+            body: "🔥 Roast incoming 😂",
+            attachment: fs.createReadStream(img)
+          },
+          event.threadID,
+          () => fs.unlinkSync(img),
+          event.messageID
+        );
+      });
+
+    } catch (err) {
+      console.log(err);
+      api.sendMessage("❌ Roast image generate করতে সমস্যা হয়েছে!", event.threadID);
+    }
+  }
+};
