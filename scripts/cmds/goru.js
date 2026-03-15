c17c315f15c7746bd8723beda3ba63397adf44dd
Md Mamun Islam <mamunofficial3210@gmail.com>
2026-03-16 04:55:45 +0600

    Create goru.js

---
+++ scripts/cmds/goru.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "goru",
+    version: "1.0",
+    author: "Mamun",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Goru troll",
+    longDescription: "Mention করলে গরুর ছবি পাঠাবে",
+    category: "fun",
+    guide: "{pn} @mention"
+  },
+
+  onStart: async function ({ api, event }) {
+    const mention = Object.keys(event.mentions)[0];
+    if (!mention) return api.sendMessage("⚠️ একজনকে মেনশন করো!", event.threadID);
+
+    const name = event.mentions[mention].replace("@","");
+
+    const img = (await axios.get("https://i.imgur.com/4AiXzf8.jpeg", { responseType: "stream" })).data;
+
+    api.sendMessage({
+      body: `🤣🐮 @${name} একদম আসল গরু হয়ে গেছে বিদেশী গরু! ✨`,
+      mentions: [{
+        id: mention,
+        tag: `@${name}`
+      }],
+      attachment: img
+    }, event.threadID);
+  }
+};
